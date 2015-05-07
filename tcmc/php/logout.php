90b67f21dcda05ce1bda4c461a705f177e61f2ec
jedixo <Fordsrock1>
2015-05-07 21:59:57 +1000

    deleted old tcmc

--- tcmc/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-  session_start();
-  unset($_SESSION['username']); // remove individual session var
-  session_destroy();
-  header('location:../index.php'); // redirct to certain page now
-?>
