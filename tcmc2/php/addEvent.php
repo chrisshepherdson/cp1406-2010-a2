19e8db30e6f7397c971b415e7f82cee3cd4a6307
jedixo <jake.dixon@my.jcu.edu.au>
2015-05-19 19:18:02 +1000

    added modal event dialog

--- tcmc2/php/addEvent.php
+++ tcmc2/php/addEvent.php
@@ -10,18 +10,21 @@ session_start()
         <form id='addevent' name='addevent' method='post' action='php/addeventprocess.php' enctype='multipart/form-data'>
             <label class='nameLabel' for='name'>Event Name    </label>
             <input name='name' class='name' type='text' id='name' placeholder='Event Name' required><br>
-            <label for='details'>Details *</label><br>
+            <label for='details'>Details:</label><br>
             <textarea name='details' rows='3' cols='48' id='details' required ></textarea><br>
             <table style='width:1'>
                 <tr>
-                    <td><label class='webpageLabel' for='date'>Date: (YYYY-MM-DD) </label></td>
-                    <td><input name='date' class='date' type='date' id='date' placeholder='Date' required> </td>
+                    <td><label class='webpageLabel' for='location'>Location:</label></td>
+                    <td><input name='location' class='location' type='text' id='location' placeholder='Location' required></td>
                 </tr>
                 <tr>
-                    <td><label class='webpageLabel' for='time'>Time:</label></td>
-                    <td><input name='time' class='time' type='time' id='time' placeholder='Time' required></td>
+                    <td><label class='webpageLabel' for='date'>Date (YYYY-MM-DD): </label></td>
+                    <td><input name='date' class='date' type='date' id='date' placeholder='YYYY-MM-DD' required> </td>
                 </tr>
-                
+                <tr>
+                    <td><label class='webpageLabel' for='time'>Time (HH:MM AM/PM):</label></td>
+                    <td><input name='time' class='time' type='time' id='time' placeholder='HH:MM' required></td>
+                </tr>
             </table>
             <input name= 'submit' class='submit' type='submit' value='Add Artist' >            
          </form>  
