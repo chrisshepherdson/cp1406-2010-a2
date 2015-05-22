9ad67b93685ce4eb4ed06f9de180a193df270947
HAO-AO <holly.otoole@my.jcu.edu.au>
2015-05-22 14:44:22 +1000

    changes trying to fix the slider

--- tcmc2/index.php
+++ tcmc2/index.php
@@ -8,14 +8,13 @@ include("php/dbconnect.php");
 <html>
 <head>
 <meta charset="utf-8">
-<title>Townsville Community Music Centre - Artists</title>
+<title>Townsville Community Music Centre - Home</title>
 <meta name="description" content="With the support of the Townsville City Council, we work from an office in the Civic ... All private schools and most government schools have music teachers.">
 <link rel="stylesheet" href="css/style.css">
 <link href="http://fonts.googleapis.com/css?family=Oswald:400,300,700" rel="stylesheet" type="text/css">
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic" rel="stylesheet" type="text/css">
 <!-- JS -->
 <script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/jquery.royalslider.min.js"></script>
 </head>
 
 <body>
@@ -274,6 +273,9 @@ The Council also assists with the performance venues for our concerts and worksh
   </div>                                                                                                    
 
 <script type="text/javascript" src="http://arrow.scrolltotop.com/arrow30.js"></script>
+    <script src="js/libs/semantic.min.js"></script>
+    <script src="js/global.js"></script>
+    <script src="js/libs/jquery.royalslider.min.js"></script>
 
   <!-- END .site-content -->
   <?php
