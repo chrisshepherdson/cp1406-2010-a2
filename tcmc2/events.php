bc60b4874b8e462aac5c8e17de750258bac20a9b
jedixo <jake.dixon@my.jcu.edu.au>
2015-05-29 13:21:58 +1000

    Revert "Applied source formatting"
    This reverts commit 45b56bccf6dc752e0d86aedb9e106cc616b1b035.

--- tcmc2/events.php
+++ tcmc2/events.php
@@ -28,33 +28,39 @@ tinyMCE.init({
     
 });
 </script>
+
 </head>
 <!-- START Logo -->
 <body>
+    
 <?php
     require("php/header.php");
 ?>
 
 <!-- WEBSITE LAYOUT Box -->
 <div id="boxed_content" class="boxed_content">
-<div class="inner">
+    <div class="inner">
 <div class="content">
-  <div class="page-innter">
+    <div class="page-innter">
     <div class="">
-      <header class="page-header">
-        <h1 class="page-title">Events</h1>
-      </header>
-      <div class="content">
-        <?php
+        <header class="page-header">
+            <h1 class="page-title">Events</h1>
+        </header>
+    
+        
+        <div class="content">
+<?php
     if ($_SESSION['username'] == "Administrator") {
         echo    "<div class='artist-button'><a href='#addEvent' class='ui small button colored'>Add new Event</a></div>";
         include("php/addEvent.php");
 
 }
-?>
-        <section class="events">
-          <ul class="events-list">
-            <?php 
+?> 
+
+
+<section class="events">
+    <ul class="events-list">
+<?php 
 $sql = "SELECT * FROM events ORDER BY time";
 foreach ($dbh->query($sql) as $row) {
     $artist;
@@ -66,38 +72,45 @@ foreach ($dbh->query($sql) as $row) {
        $date = date('l jS \of F Y h:i:s A', $row['time']);
 
 ?>
-            <li>
-              <div class="event-container">
+
+
+        <li>
+            <div class="event-container">
                 <div class="artist-image"><img <?php echo "src='$artist[thumb]' alt='$artist[name]'";?> /></div>
                 <div class="event-info">
-                  <h3 class="event-info-name"><?php echo "$row[name]"; ?></h3>
-                  <div class="event-info-details"> <span class="event-time"><strong><?php echo "$date"; ?></strong></span> <span class="event-location"> at the <?php echo "$row[location]"; ?></span> </div>
-                  <div class="artist-info-bio"><?php echo "$row[details]"; ?><br>
-                    <?php  
+                    <h3 class="event-info-name"><?php echo "$row[name]"; ?></h3>
+                    <div class="event-info-details"> 
+                        <span class="event-time"><strong><?php echo "$date"; ?></strong></span>  
+                        <span class="event-location"> at the <?php echo "$row[location]"; ?></span>                         </div>
+                    <div class="artist-info-bio"><?php echo "$row[details]"; ?><br><?php  
         if ($_SESSION[username] == "Administrator") { ?>
-                    <div class="event-button"><a href="?name=<?php echo"$row[name]"; ?>#editEvent" class="ui small button colored">user edit</a></div>
-                    <?php 
+                            
+                                <div class="event-button"><a href="?name=<?php echo"$row[name]"; ?>#editEvent" class="ui small button colored">user edit</a></div> <?php 
                     include("php/editeventdialog.php");
                                                } 
-                            ?>
-                  </div>
-                  <br>
-                </div>
-              </div>
-            </li>
-            <?php
+                            ?></div>
+                    <br>
+					
+                    
+                </div> 
+            </div>
+        </li>
+                        
+<?php
     }
 }
 ?>
-          </ul>
-        </section>
-      </div>
-      <script type="text/javascript" src="http://arrow.scrolltotop.com/arrow30.js"></script>
-      <?php
+</ul>
+</section>
+        </div>
+<script type="text/javascript" src="http://arrow.scrolltotop.com/arrow30.js"></script>
+
+<?php
 require("php/footer.php");
 ?>
     </div>
-  </div>
 </div>
+</div>
+    
 </body>
 </html>
--- tcmc2/test.php
+++ tcmc2/test.php
@@ -1,10 +1,9 @@
 <?php
 include("php/dbconnect.php");
 ?>
-
 <section class="events">
-  <ul class="events-list">
-    <?php 
+    <ul class="events-list">
+<?php 
 $sql = "SELECT * FROM events";
 foreach ($dbh->query($sql) as $row) {
     $artist;
@@ -16,21 +15,28 @@ foreach ($dbh->query($sql) as $row) {
        $date = date('l jS \of F Y h:i:s A', $row['time']);
 
 ?>
-    <li>
-      <div class="event-container">
-        <div class="event-image"><img <?php echo "src='$artist[thumb]' alt='$artist[name]'";?>></img></div>
-        <div class="event-info">
-          <h3 class="event-info-name"><?php echo "$row[name]"; ?></h3>
-          <div class="event-info-details"> <span class="event-time"><?php echo "$date"; ?></span> <span class="event-location"> at the <?php echo "$row[location]"; ?></span> </div>
-          <div class="event-info-text"><?php echo "$row[details]"; ?></div>
-          <br>
-          <div class="event-button">< <a href="#" class="ui small button coloured">Read More</a> </div>
-        </div>
-      </div>
-    </li>
-    <?php
+
+
+        <li>
+            <div class="event-container">
+                <div class="event-image"><img <?php echo "src='$artist[thumb]' alt='$artist[name]'";?>></img></div>
+                <div class="event-info">
+                    <h3 class="event-info-name"><?php echo "$row[name]"; ?></h3>
+                    <div class="event-info-details"> 
+                        <span class="event-time"><?php echo "$date"; ?></span>  
+                        <span class="event-location"> at the <?php echo "$row[location]"; ?></span>                         </div>
+                    <div class="event-info-text"><?php echo "$row[details]"; ?></div>
+                    <br>
+                    <div class="event-button"><
+                        <a href="#" class="ui small button coloured">Read More</a>
+                    </div>
+                </div> 
+            </div>
+        </li>
+                        
+<?php
     }
 }
 ?>
-  </ul>
+</ul>
 </section>
